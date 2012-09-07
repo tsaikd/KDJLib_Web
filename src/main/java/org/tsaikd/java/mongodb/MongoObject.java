9557822a344000a044dc6feed23125fbcd3623fb
tsaikd <cheng-feng.tsai@foxconn.com>
2012-09-07 10:34:00 +0800

    add MongoObject.equals(), hashCode()

--- src/main/java/org/tsaikd/java/mongodb/MongoObject.java
+++ src/main/java/org/tsaikd/java/mongodb/MongoObject.java
@@ -2,6 +2,8 @@ package org.tsaikd.java.mongodb;
 
 import java.util.List;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -183,6 +185,16 @@ public class MongoObject {
 		} else {
 			return toDBObject().toString();
 		}
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj);
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
 	}
 
 	public boolean isFetched = false;
